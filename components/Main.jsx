4b2cbea0dcddff60dfa1e68f70baaee770966460
Alonso <alosalasv@gmail.com>
2024-12-13 12:18:22 -0300

    chore(movie-app): adding pagination

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,26 +4,51 @@ import { fetchMovies } from "../lib/theMovieDb";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { MovieCard } from "./MovieCard";
 import { Screen } from "./Screen";
+import uniqBy from "lodash/uniqBy";
 
 export function Main() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const loadMovies = async (nextPage = 1) => {
+    if (loading || nextPage > totalPages) return;
+
+    setLoading(true);
+
+    const {
+      movies: newMovies,
+      page: currentPage,
+      totalPages: fetchedTotalPages,
+    } = await fetchMovies(nextPage);
+
+    setMovies((prevMovies) => uniqBy([...prevMovies, ...newMovies], "id"));
+    setPage(currentPage);
+    setTotalPages(fetchedTotalPages);
+
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetchMovies().then((movies) => {
-      setMovies(movies);
-    });
+    loadMovies();
   }, []);
 
   return (
     <Screen>
-      {movies.length === 0 ? (
+      {movies.length === 0 && loading ? (
         <ActivityIndicator color={"#fff"} size={"large"} />
       ) : (
         <FlatList
           data={movies}
-          keyExtractor={(movie) => movie.id}
+          keyExtractor={(movie) => movie.id.toString()}
           numColumns={2}
           renderItem={({ item }) => <MovieCard movie={item} />}
+          onEndReached={() => loadMovies(page + 1)}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={
+            loading ? <ActivityIndicator color={"#fff"} size={"large"} /> : null
+          }
         />
       )}
     </Screen>
